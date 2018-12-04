ff333c7e930c65e457ef41ef72abd1e56ec0c6f5
7Tanvir <tan_a@bluewin.ch>
2018-12-04 16:09:27 +0100

    Fixed problem with __main__ and uwsgi (db init)

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app.secret_key = 'Tanvir'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-	db.create_all()
 	
 jwt = JWT(app, authen, identity) # entry point /auth
 	
